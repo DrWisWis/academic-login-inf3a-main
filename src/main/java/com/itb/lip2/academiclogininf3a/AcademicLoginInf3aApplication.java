942534856a9f0cef6174893a2f92f227eeb0aad7
Igor Ibiapino dos Santos <rm87373@estudante.fieb.edu.br>
2024-02-20 20:07:25 -0300

    Criação do Controller

---
+++ src/main/java/com/itb/lip2/academiclogininf3a/AcademicLoginInf3aApplication.java
@@ -0,0 +1,13 @@
+package com.itb.lip2.academiclogininf3a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicLoginInf3aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicLoginInf3aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/lip2/academiclogininf3a/AcademicLoginInf3aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.lip2.academiclogininf3a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicLoginInf3aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
